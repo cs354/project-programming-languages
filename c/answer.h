a89176e0f9f5d7697d16b6ec19ededdd5d9c3485
n8ta <n8@u.northwestern.edu>
2020-04-29 23:07:57 -0500

    add header + import

--- c/answer.h
+++ c/answer.h
@@ -7,7 +7,7 @@
 * value printed in little-endian (also left padded and prefixed).  These fields should
 * appear in order as listed and be separated by | (the pipe character).
 */
-#import <stdio.h>
+#include <stdio.h>
 int put_stuff_in_buffer(char* buffer, int num);
 
 int put_stuff_in_buffer(char* buffer, int num) {
